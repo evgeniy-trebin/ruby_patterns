63c82b0a141ea6d03829c1ba769f935881270b4c
evgeniy-trebin <evgeniy.trebin@gmail.com>
2015-11-09 11:12:52 +0300

    .

--- behavioral_patterns/iterator/example_1_inventory/index.rb
+++ behavioral_patterns/iterator/example_1_inventory/index.rb
@@ -2,18 +2,19 @@ require_relative 'inventory'
 require_relative 'item'
 require_relative 'inventory_iterator'
 
-item0 = Item.new('Item0', 100)
-item1 = Item.new('Item1', 10)
-item2 = Item.new('Item2', 20)
+item0 = Item.new('АК-47', 2700)
+item1 = Item.new('Helmet', 650)
+item2 = Item.new('Deagle', 800)
 
 inventory = Inventory.new
-inventory.add(item2)
 inventory.add(item0)
 inventory.add(item1)
+inventory.add(item2)
 
 iterator = InventoryIterator.new(inventory)
 p iterator.next.title
 p iterator.next.title
+p iterator.next.title
 
-p inventory.max
-p inventory.min
+p inventory.max.title
+p inventory.min.title
